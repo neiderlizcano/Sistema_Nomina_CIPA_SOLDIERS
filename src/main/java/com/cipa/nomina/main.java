a247df1fec3dcc7dd856fbc1019153477a8dcf96
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2026-05-18 18:28:33 -0500

    chore: configurar estructura base del proyecto

---
+++ src/main/java/com/cipa/nomina/main.java
@@ -0,0 +1,8 @@
+package com.cipa.nomina;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Nómina CIPA iniciado correctamente.");
+    }
+}
